f70d2ff7eca3bcf6941a6720a747358aaf085d33
Anurag <anuragyadav2516@gmail.com>
2025-11-19 23:27:33 +0530

    style: adjust spacing issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 w-full z-50 bg-slate-900/80 backdrop-blur-md border-b border-white/10">
-      <div className="mx-auto max-w-7xl px-6 py-4 flex items-center justify-between">
+      <div className="mx-auto max-w-7xl px-6 py-5 flex items-center justify-between">
         <h1 className="text-slate-50 font-semibold text-2xl">LaunchFlow</h1>
 
         <ul className="hidden md:flex items-center gap-10 text-slate-300 font-semibold text-lg">
@@ -47,7 +47,7 @@ const Navbar = () => {
 
         <button
           type="button"
-          className="hidden md:block bg-linear-to-r from-indigo-500 to-purple-500 text-slate-50 text-base font-semibold px-4 py-2 rounded-lg transition-all duration-300 hover:scale-105 hover:brightness-110 hover:shadow-lg hover:shadow-purple-500/50"
+          className="hidden md:block cursor-pointer bg-linear-to-r from-indigo-500 to-purple-500 text-slate-50 text-base font-semibold px-4 py-2 rounded-lg transition-all duration-300 hover:scale-105 hover:brightness-110 hover:shadow-lg hover:shadow-purple-500/50"
         >
           Get Started
         </button>
